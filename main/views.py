0d6c5aeecae8ec28499737ddd21f67f91a6a2b7f
Nazarkooo <makaryshynnazar3@gmail.com>
2025-05-16 14:00:32 +0300

    Add CV detail view, template, and update CV list to link to details

--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import CVModel
 
 class CVListView(ListView):
@@ -8,3 +8,12 @@ class CVListView(ListView):
 
     def get_queryset(self):
         return CVModel.objects.prefetch_related('skills', 'projects', 'contacts').all()
+
+class CVDetailView(DetailView):
+    model = CVModel
+    template_name = 'main/cv_detail.html'
+    context_object_name = 'cv'
+
+    def get_queryset(self):
+        return CVModel.objects.prefetch_related('skills', 'projects', 'contacts')
+    
